8d256197f65b6369dfbcb973842cec3788a0af59
James Coates <james.coates99@hotmail.com>
2022-10-09 23:21:59 +0100

    Consuming project uses local package

--- consuming-project/src/index.ts
+++ consuming-project/src/index.ts
@@ -1,9 +1,12 @@
 import { message } from 'example-ts-npm-package';
+import { message as localPackageMessage } from 'local-package';
 
 console.log('*********************************');
 console.log('This is the consuming project\n');
 
-message('This is consuming the typescript package');
+message('This is consuming the published typescript package');
+console.log();
+localPackageMessage('This is consuming the local package');
 
 console.log('\nConsuming project complete');
 console.log('*********************************');
