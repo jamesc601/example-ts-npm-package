6e22427c1d2f685db83b9f27b75589d37fa2e04d
James Coates <james.coates99@hotmail.com>
2022-10-09 22:26:51 +0100

    Created consuming project

---
+++ consuming-project/src/index.ts
@@ -0,0 +1,9 @@
+import { message } from 'example-ts-npm-package';
+
+console.log('*********************************');
+console.log('This is the consuming project\n');
+
+message('This is consuming the typescript package');
+
+console.log('\nConsuming project complete');
+console.log('*********************************');
