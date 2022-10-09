eb31607b12066a37fb7192b2f4b8c66ada27c024
James Coates <james.coates99@hotmail.com>
2022-10-09 22:44:01 +0100

    Consume local packages

--- src/test-file.ts
+++ src/test-file.ts
@@ -1,9 +1,12 @@
 import { message } from './index';
+import { message as packageMessage } from 'local-package';
 
 console.log('*********************************');
 console.log('Starting the test\n');
 
 message('This uses the function directly');
+console.log();
+packageMessage('This uses the local package');
 
 console.log('\nTest completed');
 console.log('*********************************');
