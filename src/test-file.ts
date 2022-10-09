0ab49146e65acc205b81be32d4b6e24290586b1c
James Coates <james.coates99@hotmail.com>
2022-10-09 22:14:17 +0100

    Add test file

---
+++ src/test-file.ts
@@ -0,0 +1,9 @@
+import { message } from './index';
+
+console.log('*********************************');
+console.log('Starting the test\n');
+
+message('This uses the function directly');
+
+console.log('\nTest completed');
+console.log('*********************************');
