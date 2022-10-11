bc23a818b16544fcb28569f04cd2abbc5a3c85a1
James Coates <james.coates99@hotmail.com>
2022-10-11 13:43:53 +0100

    Remove local package stuff from consuming project

--- consuming-project/src/index.ts
+++ consuming-project/src/index.ts
@@ -1,12 +1,9 @@
 import { message } from '@jamescoates/example-ts-npm-package-verdaccio';
-import { message as localPackageMessage } from 'local-package';
 
 console.log('*********************************');
 console.log('This is the consuming project\n');
 
 message('This is consuming the published typescript package');
-console.log();
-localPackageMessage('This is consuming the local package');
 
 console.log('\nConsuming project complete');
 console.log('*********************************');
